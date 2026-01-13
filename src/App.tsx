ad5ba56a90073c76646f176c0e80cde1e4c452eb
prateeksaxena1 <pksaxena6453@gmail.com>
2026-01-13 13:51:52 +0530

    black screen issue fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route, useLocation } from 'react-router-dom';
-import { AnimatePresence, motion } from 'framer-motion';
+
 import { Layout } from './components/layout/Layout';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
@@ -21,65 +21,55 @@ function App() {
 
   return (
     <Layout>
-      <AnimatePresence mode="wait">
-        <motion.div
-          key={location.pathname}
-          initial={false} // Prevent black screen on first load
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.3 }}
-        >
-          <Routes location={location} key={location.pathname}>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/themes" element={<Themes />} />
-            <Route path="/prizes" element={<PrizesPage />} />
-            <Route path="/partners" element={<Partners />} />
-            <Route path="/speakers" element={<SpeakersPage />} />
-            <Route
-              path="/itinerary"
-              element={
-                <>
-                  <ComingSoonOverlay>
-                    <Itinerary />
-                  </ComingSoonOverlay>
-                  <PageNavigation />
-                </>
-              }
-            />
-            <Route
-              path="/gallery"
-              element={
-                <>
-                  <Gallery />
-                  <PageNavigation />
-                </>
-              }
-            />
-            <Route
-              path="/faq"
-              element={
-                <>
-                  <FAQPage />
-                  <PageNavigation />
-                </>
-              }
-            />
-            <Route path="/events"
-              element={
-                <>
-                  <ComingSoonOverlay>
-                    <EventsPage />
-                  </ComingSoonOverlay>
-                  <PageNavigation />
-                </>
-              }
-            />
-            <Route path="/sponsor" element={<WhySponsor />} />
-            <Route path="/cloud-test" element={<CloudTest />} />
-          </Routes>
-        </motion.div>
-      </AnimatePresence>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/themes" element={<Themes />} />
+        <Route path="/prizes" element={<PrizesPage />} />
+        <Route path="/partners" element={<Partners />} />
+        <Route path="/speakers" element={<SpeakersPage />} />
+        <Route
+          path="/itinerary"
+          element={
+            <>
+              <ComingSoonOverlay>
+                <Itinerary />
+              </ComingSoonOverlay>
+              <PageNavigation />
+            </>
+          }
+        />
+        <Route
+          path="/gallery"
+          element={
+            <>
+              <Gallery />
+              <PageNavigation />
+            </>
+          }
+        />
+        <Route
+          path="/faq"
+          element={
+            <>
+              <FAQPage />
+              <PageNavigation />
+            </>
+          }
+        />
+        <Route path="/events"
+          element={
+            <>
+              <ComingSoonOverlay>
+                <EventsPage />
+              </ComingSoonOverlay>
+              <PageNavigation />
+            </>
+          }
+        />
+        <Route path="/sponsor" element={<WhySponsor />} />
+        <Route path="/cloud-test" element={<CloudTest />} />
+      </Routes>
     </Layout>
   );
 }
